087434196b7c7682071ca04a8ace976a457be4be
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-15 22:19:09 +0800

    start a new topic

--- test/integration/topics_integration_test.rb
+++ test/integration/topics_integration_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class TopicsIntegrationTest < ActionDispatch::IntegrationTest
+  def setup
+  end
+
+  def teardown
+    Topic.destroy_all
+  end
+
   test 'should display all topics' do
     topic1 = Topic.create title: "Rails 4 has been released!", text: "I love RoR!", link: "http://rubyonrails.org/"
     topic2 = Topic.create title: "Ruby 2 has been released!", text: "I love RoR!", link: "http://ruby-lang.org/"
@@ -8,5 +15,19 @@ class TopicsIntegrationTest < ActionDispatch::IntegrationTest
     visit root_path
     assert page.has_content?(topic1.title)
     assert page.has_content?(topic2.title)
+  end
+
+  test 'should start a new topic' do
+    topic = Topic.new title: "Rails 4 has been released!", text: "I love RoR!", link: "http://rubyonrails.org/"
+    visit root_path
+    refute page.has_content?(topic.title)
+    
+    click_link 'Start A Topic'
+    fill_in 'Title', with: topic.title
+    fill_in 'Text', with: topic.text
+    fill_in 'Link', with: topic.link
+    click_button 'Submit'
+
+    assert page.has_content?(topic.title)
   end
 end
