c94fff949cca7a3ef8fc1bc9f7a0400fcd871110
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-15 21:28:02 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kopitiam::Application.config.secret_key_base = 'd0d094377f4729698f36b96eab79190ce02e5c05d7870bea6eacd65d67600bfc9529e8f985a37a7e1e34896bf20f38ebc4c08d654e5dc273792da89acdcc9a10'
