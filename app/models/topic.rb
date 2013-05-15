65fb94d477751b69431b8c517c70c1eff3a11c5f
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-15 22:27:13 +0800

    topic must have title

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,3 @@
 class Topic < ActiveRecord::Base
+  validates :title, presence: true
 end
--- test/integration/topics_integration_test.rb
+++ test/integration/topics_integration_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class TopicsIntegrationTest < ActionDispatch::IntegrationTest
   def setup
+    @topic1 = Topic.new title: "Rails 4 has been released!", text: "I love RoR!", link: "http://rubyonrails.org/"
+    @topic2 = Topic.new title: "Ruby 2 has been released!", text: "I love RoR!", link: "http://ruby-lang.org/"
   end
 
   def teardown
@@ -9,25 +11,36 @@ class TopicsIntegrationTest < ActionDispatch::IntegrationTest
   end
 
   test 'should display all topics' do
-    topic1 = Topic.create title: "Rails 4 has been released!", text: "I love RoR!", link: "http://rubyonrails.org/"
-    topic2 = Topic.create title: "Ruby 2 has been released!", text: "I love RoR!", link: "http://ruby-lang.org/"
-    
+    @topic1.save && @topic2.save
     visit root_path
-    assert page.has_content?(topic1.title)
-    assert page.has_content?(topic2.title)
+    assert page.has_content?(@topic1.title)
+    assert page.has_content?(@topic2.title)
   end
 
   test 'should start a new topic' do
-    topic = Topic.new title: "Rails 4 has been released!", text: "I love RoR!", link: "http://rubyonrails.org/"
     visit root_path
-    refute page.has_content?(topic.title)
+    refute page.has_content?(@topic1.title)
     
     click_link 'Start New Topic'
-    fill_in 'Title', with: topic.title
-    fill_in 'Text', with: topic.text
-    fill_in 'Link', with: topic.link
+    fill_in 'Title', with: @topic1.title
+    fill_in 'Text', with: @topic1.text
+    fill_in 'Link', with: @topic1.link
     click_button 'Submit'
 
-    assert page.has_content?(topic.title)
+    assert page.has_content?(@topic1.title)
+  end
+
+  test 'should fail to start a new topic without title' do
+    visit root_path
+    before_count = Topic.count
+    
+    click_link 'Start New Topic'
+    fill_in 'Title', with: ''
+    fill_in 'Text', with: @topic1.text
+    fill_in 'Link', with: @topic1.link
+    click_button 'Submit'
+
+    assert page.has_content?('error')
+    assert_equal before_count, Topic.count
   end
 end
