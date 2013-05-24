fb66f2ad8a1f145c0fa59c68b69778cbf0f36fd6
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-24 15:27:43 +0800

    configure strong param for devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
 end
