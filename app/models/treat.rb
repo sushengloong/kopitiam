d16a45636a830968ae68457535669a575e8aa142
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-27 22:24:42 +0800

    refresh topic's treat score when a treat is created

--- app/models/treat.rb
+++ app/models/treat.rb
@@ -1,4 +1,6 @@
 class Treat < ActiveRecord::Base
+  after_create :refresh_topic_treat_score
+
   belongs_to :user
   belongs_to :topic
 
@@ -10,5 +12,12 @@ class Treat < ActiveRecord::Base
     MILO = 'milo'
     BARLEY = 'barley'
     BEER = 'beer'
+  end
+
+  private
+
+  def refresh_topic_treat_score
+    topic.treat_score += value
+    topic.save
   end
 end
