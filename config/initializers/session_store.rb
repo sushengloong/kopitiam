7c5e22bfa847f89107d66911a02b0377bdcb1698
Sheng-Loong Su <sushengloong@gmail.com>
2013-06-15 08:14:56 +0800

    update rails to 4.0.0.rc2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Kopitiam::Application.config.session_store :encrypted_cookie_store, key: '_kopitiam_session'
+# changed session store from :encrypted_cookie_store to :cookie_store
+# to resolve an issue after updating to rails 4.0.0.rc2
+# refer to http://stackoverflow.com/a/15849479/626687
+Kopitiam::Application.config.session_store :cookie_store, key: '_kopitiam_session'
